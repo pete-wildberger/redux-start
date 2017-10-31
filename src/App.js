e9c991dee680824a055e7840102252e90a65eca7
Peter Wildberger <pete.wildberger@gmail.com>
2017-10-31 11:41:20 -0500

    ready to start

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store';
 import logo from './logo.svg';
-import './App.css';
+import './styles/main.min.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+const FourOhFour = () => <h1>404</h1>;
+
+const App = () => (
+  <BrowserRouter>
+    <Provider store={store}>
+      <div className="app">
+        <Switch />
       </div>
-    );
-  }
-}
-
+    </Provider>
+  </BrowserRouter>
+);
 export default App;
