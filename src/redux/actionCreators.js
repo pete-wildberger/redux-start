ef08f0c01b22d23f2cabab0a62ce17db21c7ff1e
Peter Wildberger <pete.wildberger@gmail.com>
2017-11-01 08:49:28 -0500

    readier to go

--- src/redux/actionCreators.js
+++ src/redux/actionCreators.js
@@ -1,5 +1,22 @@
 import { SET_SEARCH_TERM } from './actions';
+import { ADD_API_DATA } from './actions';
+import axios from 'axios';
 
 export function setSearchTerm(searchTerm) {
   return { type: SET_SEARCH_TERM, payload: searchTerm };
+}
+export function addApiData(apiData) {
+  return { type: ADD_API_DATA, payload: apiData };
+}
+export function getApiDetails(imdbID) {
+  return dispatch => {
+    axios
+      .get(`http://localhost:3000/${imdbID}`)
+      .then(response => {
+        dispatch(addApiData(response.data));
+      })
+      .catch(err => {
+        console.error('axios error ', err);
+      });
+  };
 }
